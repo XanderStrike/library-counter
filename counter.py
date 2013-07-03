7da0cd6fceeacea09607d30226df77006889be75
xanderstrike <xanderstrike@gmail.com>
2013-07-03 14:04:16 -0700

    add sqlite3 stuff to server

--- counter.py
+++ counter.py
@@ -1,3 +1,6 @@
+#counter.py
+# Handles counting the switch connections.
+
 # imports
 import time
 import RPi.GPIO as GPIO
@@ -9,7 +12,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 con = lite.connect('db')
 
-# count = 0
+count = 0
 
 # endless loop, does really well at mitigating bounce
 prev_input = 0
@@ -19,7 +22,7 @@ while True:
     cur = con.cursor()
     cur.execute("INSERT INTO Times VALUES(" + str(time.time()) + ")")
     con.commit()
-    # count += 1
-    # print count
+    count += 1
+    print "[Counter] Another! That makes: " + str(count)
   prev_input = input
   time.sleep(.05)
--- server.py
+++ server.py
@@ -1,22 +1,42 @@
+#server.py
+# Serves information for the api and the web page
+
 from bottle import route, run, template
+import sqlite3 as lite
+import sys
+import time
+import json
 
-@route('/hello/<name>')
-def index(name='World'):
+con = lite.connect('db')
+
+@route('/api/<period>')
+@route('/api/<period>.json')
+def api(period='day'):
+  cur = con.cursor()
+  mod = 1
+  if period == 'hour':
+    mod = 3600
+  elif period == 'day':
+    mod = 86400
+  elif period == 'week':
+    mod = 604800
+  elif period == 'month':
+    mod = 2592000
+  elif period == 'year':
+    mod = 31536000
+  elif period == 'all':
+    cur.execute("SELECT * FROM Times")
+    return json.dumps(cur.fetchall())
+  
+  cur.execute("SELECT * FROM Times WHERE Time > " + str(time.time() - mod))
+  return json.dumps(cur.fetchall())
+
+@route('/')
+def index():
+  cur = con.cursor()
+  cur.execute("SELECT COUNT(*) FROM Times WHERE Time > " + (str(time.time() - (time.time() % 86400)+ 25200)))
+  count = cur.fetchall()[0][0]
   return template('''
-<html>
-<head>
-  <title>This is a page.</title>
-  <style>body {margin:0px 0px 0px 0px;}#main {background:url(http://24.media.tumblr.com/tumblr_linam9F4Tg1qz6s22o1_1280.jpg);background-size:cover;background-position:center bottom;} .inner {background: rgba(0,0,0,0.2) url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAAECAYAAACp8Z5+AAAAGklEQVQIW2NkYGD4D8SMQAwGcAY2AbBKDBUAVuYCBQPd34sAAAAASUVORK5CYII=) repeat; padding:10px 10px;  color:#eee;  min-height: calc(100% - 20px);}
-</style>
-</head>
-<body>
-  <section id="main">
-    <div class="inner">
- 	{{name}} 
-    </div>
-  </section>
-</body>
-</html>
-''', name=name)
+<html><head><title>Library Count</title><link href='http://fonts.googleapis.com/css?family=Advent+Pro:400,300' rel='stylesheet' type='text/css'><style>body {margin:0px 0px 0px 0px;}#main {background:url(http://24.media.tumblr.com/tumblr_linam9F4Tg1qz6s22o1_1280.jpg);background-size:cover;background-position:center bottom;}.inner {background: rgba(0,0,0,0.2) url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAQAAAAECAYAAACp8Z5+AAAAGklEQVQIW2NkYGD4D8SMQAwGcAY2AbBKDBUAVuYCBQPd34sAAAAASUVORK5CYII=) repeat;padding:10px 10px;color:#eee;min-height: calc(100% - 20px);}h1 {font-family: 'Advent Pro', sans-serif;font-size:200px;text-align:center;}</style><script src="http://code.jquery.com/jquery-1.10.1.min.js"></script><script>$(window).load(function() {var $windowHeight = $(window).height() - 50;$('h1').css({'margin-top' : (($windowHeight) - $('h1').outerHeight())/2,'margin-bottom' : (($windowHeight) - $('h1').outerHeight())/2,'opacity' : '1.0','filter' : 'alpha(opacity = 100)',});});</script></head><body>  <section id="main">    <div class="inner"><h1>{{output}}</h1>    </div>  </section></body></html>''', output=count)
 
-run(host='0.0.0.0', port=8080)
+run(host='0.0.0.0', port=80)
